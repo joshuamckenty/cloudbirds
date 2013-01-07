9b7367ff96b833c224e5931eec0cac8b4d5095db
Joshua McKenty <jmckenty@gmail.com>
2013-01-07 14:33:07 -0800

    Gossip now communicates dead bird detection as well.

--- cloudbirds/agent.py
+++ cloudbirds/agent.py
@@ -5,6 +5,9 @@ CloudBirds agent, runs on each VM
 
 """
 
+import json
+import random
+import requests
 from fysom import Fysom
 
 
@@ -26,20 +29,53 @@ class CloudBirdAgent(Agent):
 		})
 		
 		self.momma = None
+		self.host = "localhost"
 		self.port = str(port)
-		self.flock = ['8000']
-		self.add_flock_member(self.port)
+		self.flock = ['http://localhost:8000']
+		self.antiFlock = []
+		self.add_flock_member(self.url)
 	
-	def add_flock_member(self, bird):
-		if not bird in self.flock:
-			self.flock.append(bird)
+	@property
+	def url(self):
+		return "http://%s:%s" % (self.host, self.port)
 	
-	def mainloop(self):
-		# Poll my current system state via snmp
-		# Gossip a bit
-		# Maybe kill my children?
-		# Maybe change state?
-		pass
+	def add_flock_member(self, bird_url):
+		if not bird_url in self.flock:
+			self.flock.append(bird_url)
+		if bird_url in self.antiFlock:
+			self.antiFlock.remove(bird_url)
+	
+	def remove_flock_member(self, bird_url):
+		if bird_url in self.flock:
+			self.flock.remove(bird_url)
+		if not bird_url in self.antiFlock:
+			self.antiFlock.append(bird_url)
+	
+	def gossip(self):
+		bird_url = random.choice(self.flock)
+		if bird_url == self.url:
+			return "Don't like to talk to myself."
+		url = "%s/tellme/%s" % (bird_url, self.port)
+		actions = [{'action': 'saw_bird', 'bird_url': saw_bird} for saw_bird in self.flock]
+		actions.extend([{'action': 'dead_bird', 'bird_url': dead_bird} for dead_bird in self.antiFlock])
+		msg = json.dumps(actions)
+		try:
+			requests.post(url, data={'msg': msg})
+		except requests.exceptions.ConnectionError, e:
+			# I don't believe in this bird anymore
+			self.remove_flock_member(bird_url)
+		return "Did it."
+	
+	def process_gossip(self, msg):
+		print "Got some gossip with msg %s" % msg
+		messages = json.loads(msg)
+		for msg in messages:
+			if msg['action'] == "saw_bird":
+				self.add_flock_member(msg['bird_url'])
+				print "Adding a flock member of %s" % msg['bird_url']
+			if msg['action'] == "dead_bird":
+				self.remove_flock_member(msg['bird_url'])
+				print "Removing a flock member of %s" % msg['bird_url']
 
 	def imprint(self):
 		pass
--- cloudbirds/bird.py
+++ cloudbirds/bird.py
@@ -4,8 +4,9 @@ Yer basic cloudbird daemon, runs on every host.
 TODO:
 Security of this interface
 """
+
 import random
-from flask import Flask, render_template, g
+from flask import Flask, request, render_template, g
 from twisted.internet import task
 from twisted.internet import reactor
 from twisted.web.wsgi import WSGIResource
@@ -13,7 +14,6 @@ from twisted.web.server import Site
 
 import agent
 import util
-import requests
 
 LOW_PORT = 8001
 HIGH_PORT = 9000
@@ -21,44 +21,21 @@ OMEGA_PORT = 8000
 
 OMEGA = False
 
-# If there's no omega, I need to be omega
-if not (util.test_for_socket(port=OMEGA_PORT)):
-	OMEGA = True
-
 def pick_port():
 	if OMEGA: return OMEGA_PORT
 	while True: # TODO - probably shouldn't run this forever
 		port=random.randrange(LOW_PORT, HIGH_PORT)
 		if not util.test_for_socket(port=port):
 			return port
-			
+
+
+# If there's no omega, I need to be omega
+if not (util.test_for_socket(port=OMEGA_PORT)):
+	OMEGA = True			
 birdport = pick_port()	
 app = Flask(__name__)
 app.debug = True
 app.myBird = agent.CloudBirdAgent(birdport)
-
-@app.route('/tellme/<bird>')
-def tell_me(bird='9001'):
-	"""Receives gossip messages from other birds in the flock
-	"""
-	app.myBird.add_flock_member(bird)
-	print "Adding a flock member of %s" % bird
-	return "Okay."
-
-@app.route('/gossip')
-def gossip():
-	"""Sends gossip messages.
-	Always sends news to the OMEGA (for now), # TODO THIS!
-	Randomly sends to other birds."""
-	target_bird = random.choice(app.myBird.flock)
-	if target_bird == app.myBird.port:
-		return "Don't like to talk to myself."
-	for bird in app.myBird.flock: # Should be random later
-		# TODO: Batch up these calls
-		url = "http://localhost:%s/tellme/%s" % (target_bird, bird)
-		print "Going to fetch against %s", url
-		requests.get(url)
-	return "Did it."
 
 
 @app.route('/')
@@ -68,22 +45,40 @@ def index(name='World'):
 	print "got request"
 	return "<b>Hello %(name)s</b>!<br/>I am an: %(state)s bird" % {'name' :name, 'state' : app.myBird.fsm.current}
 
+
+@app.route('/tellme/<frombird>', methods=['POST'])
+def tell_me(frombird='9001'):
+	"""Receives gossip messages from other birds in the flock
+	MSG is in format:
+	msg = 
+	[{'action': 'saw_bird', 'bird_url': 'URL'},
+	{'action': 'dead_bird', 'bird_url': 'URL'}]
+	"""
+	app.myBird.process_gossip(request.form['msg'])
+	return "Okay."
+
+
 @app.route('/flock')
 def report_flock():
-	return "<b>Heres what weve got: %(flock)s</b>" % {'flock': app.myBird.flock}
+	return "<b>Heres what we've got: %(flock)s</b>" % {'flock': app.myBird.flock}
+
+
+@app.route('/gossip')
+def gossip():
+	"""Sends gossip messages.
+	Randomly sends to other birds."""
+	return app.myBird.gossip()
+
 
 l = task.LoopingCall(gossip)
-l.start(3.0) # call every second
 # l.stop() will stop the looping calls
 
 resource = WSGIResource(reactor, reactor.getThreadPool(), app)
 site = Site(resource)
 print "Going to run on ", birdport
 reactor.listenTCP(birdport, site)
+l.start(3.0) # call every second
 reactor.run()
-
-# run(host='localhost', port=birdport)
-
 
 # http://highscalability.com/blog/2011/11/14/using-gossip-protocols-for-failure-detection-monitoring-mess.html
 # http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.160.2604
