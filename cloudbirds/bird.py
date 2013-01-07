409d0b2765d301c6f4819e37b4a0a397e5b256a4
Joshua McKenty <jmckenty@gmail.com>
2013-01-07 13:22:51 -0800

    Replaced bottle with flask and twisted, added timed loop to force gossiping.

--- cloudbirds/bird.py
+++ cloudbirds/bird.py
@@ -5,7 +5,12 @@ TODO:
 Security of this interface
 """
 import random
-from bottle import route, run, template
+from flask import Flask, render_template, g
+from twisted.internet import task
+from twisted.internet import reactor
+from twisted.web.wsgi import WSGIResource
+from twisted.web.server import Site
+
 import agent
 import util
 import requests
@@ -28,41 +33,58 @@ def pick_port():
 			return port
 			
 birdport = pick_port()	
-myBird = agent.CloudBirdAgent(birdport)
+app = Flask(__name__)
+app.debug = True
+app.myBird = agent.CloudBirdAgent(birdport)
 
-@route('/tellme/:bird')
+@app.route('/tellme/<bird>')
 def tell_me(bird='9001'):
 	"""Receives gossip messages from other birds in the flock
 	"""
-	myBird.add_flock_member(bird)
+	app.myBird.add_flock_member(bird)
+	print "Adding a flock member of %s" % bird
 	return "Okay."
 
-@route('/gossip')
+@app.route('/gossip')
 def gossip():
 	"""Sends gossip messages.
 	Always sends news to the OMEGA (for now), # TODO THIS!
 	Randomly sends to other birds."""
-	target_bird = random.choice(myBird.flock)
-	if target_bird == myBird.port:
+	target_bird = random.choice(app.myBird.flock)
+	if target_bird == app.myBird.port:
 		return "Don't like to talk to myself."
-	for bird in myBird.flock: # Should be random later
+	for bird in app.myBird.flock: # Should be random later
 		# TODO: Batch up these calls
 		url = "http://localhost:%s/tellme/%s" % (target_bird, bird)
 		print "Going to fetch against %s", url
 		requests.get(url)
+	return "Did it."
 
 
-@route('/')
-@route('/hello')
-@route('/hello/:name')
+@app.route('/')
+@app.route('/hello')
+@app.route('/hello/<name>')
 def index(name='World'):
 	print "got request"
-	return template('<b>Hello {{name}}</b>!<br/>I am an: {{state}} bird', name=name, state=myBird.fsm.current)
+	return "<b>Hello %(name)s</b>!<br/>I am an: %(state)s bird" % {'name' :name, 'state' : app.myBird.fsm.current}
 
-
-@route('/flock')
+@app.route('/flock')
 def report_flock():
-	return template('<b>Heres what weve got: {{flock}}</b>', flock=myBird.flock)
+	return "<b>Heres what weve got: %(flock)s</b>" % {'flock': app.myBird.flock}
+
+l = task.LoopingCall(gossip)
+l.start(3.0) # call every second
+# l.stop() will stop the looping calls
+
+resource = WSGIResource(reactor, reactor.getThreadPool(), app)
+site = Site(resource)
+print "Going to run on ", birdport
+reactor.listenTCP(birdport, site)
+reactor.run()
+
+# run(host='localhost', port=birdport)
 
 
-run(host='localhost', port=birdport)
+# http://highscalability.com/blog/2011/11/14/using-gossip-protocols-for-failure-detection-monitoring-mess.html
+# http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.160.2604
+# http://pysnmp.sourceforge.net/examples/current/index.html
